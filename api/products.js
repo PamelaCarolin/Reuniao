e1e9aea097db1f1cd5246b01b873fffa9b841b09
PamelaCarolin <pamela.silva@contabilmultipla.com.br>
2024-08-05 12:11:10 -0300

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,22 +1,20 @@
-const sqlite3 = require('sqlite3').verbose();
 const db = require('./database');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     const { category } = req.query;
     let query = "SELECT name, category FROM products";
     const queryParams = [];
 
     if (category && category !== 'Todos') {
-        query += " WHERE category = ?";
+        query += " WHERE category = $1";
         queryParams.push(category);
     }
 
-    db.all(query, queryParams, (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
+    try {
+        const { rows } = await db.query(query, queryParams);
         res.json(rows);
-    });
+    } catch (err) {
+        console.error('Erro ao buscar produtos:', err);
+        res.status(500).json({ error: 'Erro ao buscar produtos' });
+    }
 };
-
