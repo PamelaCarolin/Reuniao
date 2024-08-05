f4a732dafbf06b460a24d1783f2137447174e98d
PamelaCarolin <pamela.silva@contabilmultipla.com.br>
2024-08-05 11:37:38 -0300

    Update cancelar.js

--- api/cancelar.js
+++ api/cancelar.js
@@ -1,1 +1,14 @@
 
+const sqlite3 = require('sqlite3').verbose();
+const db = require('./database');
+
+module.exports = (req, res) => {
+    const { id } = req.body;
+    const query = `DELETE FROM meetings WHERE id = ?`;
+    db.run(query, [id], function(err) {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json({ success: true, message: 'Reunião cancelada com sucesso!' });
+    });
+};
