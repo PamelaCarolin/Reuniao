93f9291d6a46e6766e68f9b68ab378d22626ecf5
PamelaCarolin <pamela.silva@contabilmultipla.com.br>
2024-08-05 11:38:04 -0300

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,1 +1,22 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = require('./database');
+
+module.exports = (req, res) => {
+    const { category } = req.query;
+    let query = "SELECT name, category FROM products";
+    const queryParams = [];
+
+    if (category && category !== 'Todos') {
+        query += " WHERE category = ?";
+        queryParams.push(category);
+    }
+
+    db.all(query, queryParams, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json(rows);
+    });
+};
 
