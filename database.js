fa82f1f06e4767870cc67f25b65fd3702510dc62
PamelaCarolin <pamela.silva@contabilmultipla.com.br>
2024-08-05 12:10:16 -0300

    Update database.js

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'seu_usuario',          // Substitua pelo seu usuário do PostgreSQL
-  host: 'ep-sweet-night-a4g91n22-pooler',  // Substitua pelo seu host do PostgreSQL
+  host: 'psql "postgres://default:8DOfXcRSwg1h@ep-sweet-night-a4g91n22.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"',  // Substitua pelo seu host do PostgreSQL
   database: 'sua_database',     // Substitua pelo nome do seu banco de dados
   password: 'sua_senha',        // Substitua pela sua senha do PostgreSQL
   port: 5432,                   // Porta padrão do PostgreSQL
