42e68fadeb8daf205119b528cce095eeaee39076
PamelaCarolin <pamela.silva@contabilmultipla.com.br>
2024-08-05 11:21:23 -0300

    Update app.js

--- reuniao/app.js
+++ reuniao/app.js
@@ -49,6 +49,11 @@ app.post('/agendar', (req, res) => {
 // Rota para servir arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Rota para capturar todas as outras requisições e servir o index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
