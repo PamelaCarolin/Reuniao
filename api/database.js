9c3da3d7b21a64a96c9f466880bc33675598f4aa
PamelaCarolin <pamela.silva@contabilmultipla.com.br>
2024-11-08 16:56:41 -0300

    Update database.js

--- api/database.js
+++ api/database.js
@@ -1,28 +1,12 @@
-const { Pool } = require('pg');
 require('dotenv').config();
+const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
-
-pool.on('connect', () => {
-  console.log('Conectado ao banco de dados');
-});
-
-pool.on('error', (err) => {
-  console.error('Erro no banco de dados:', err);
+  ssl: { rejectUnauthorized: false } // Necessário para conexões seguras em alguns provedores
 });
 
 module.exports = {
-  query: async (text, params) => {
-    try {
-      const res = await pool.query(text, params);
-      return res;
-    } catch (error) {
-      console.error('Erro ao executar query:', error);
-      throw error;
-    }
-  },
+  query: (text, params) => pool.query(text, params),
   connect: () => pool.connect()
 };
