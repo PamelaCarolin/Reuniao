f86fdb3f16d47960f2136e8eccac41f09b7007ad
PamelaCarolin <pamela.silva@contabilmultipla.com.br>
2024-11-08 16:54:52 -0300

    Update database.js

--- api/database.js
+++ api/database.js
@@ -1,10 +1,28 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: 'postgres://default:8DOfXcRSwg1h@ep-sweet-night-a4g91n22.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require'
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+pool.on('connect', () => {
+  console.log('Conectado ao banco de dados');
+});
+
+pool.on('error', (err) => {
+  console.error('Erro no banco de dados:', err);
 });
 
 module.exports = {
-  query: (text, params) => pool.query(text, params),
+  query: async (text, params) => {
+    try {
+      const res = await pool.query(text, params);
+      return res;
+    } catch (error) {
+      console.error('Erro ao executar query:', error);
+      throw error;
+    }
+  },
   connect: () => pool.connect()
-}
+};
