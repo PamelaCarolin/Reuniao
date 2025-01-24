cfd3df633b7d7f9669968bc71b225460953d348b
PamelaCarolin <pamela.silva@contabilmultipla.com.br>
2025-01-24 11:17:05 -0300

    Update agendar.js

--- api/agendar.js
+++ api/agendar.js
@@ -1,18 +1,21 @@
 const db = require('./database');
 
 module.exports = async (req, res) => {
+    const clientDB = await db.connect();
+
     if (req.method === 'POST') {
+        // Agendamento de reunião
         const { date, time, duration, sector, speaker, room, client } = req.body;
-        const clientDB = await db.connect();
 
         try {
             await clientDB.query('BEGIN');
 
+            // Verificar conflitos de horário (exceto para Teams)
             if (room.toLowerCase() !== 'teams') {
                 const conflictQuery = `
                     SELECT * FROM meetings 
-                    WHERE date = $1 AND room = $2 AND 
-                    (
+                    WHERE date = $1 AND room = $2 
+                    AND (
                         ($3::time BETWEEN time AND time + interval '1 minute' * duration) OR 
                         ($3::time + interval '1 minute' * $4 BETWEEN time AND time + interval '1 minute' * duration)
                     )
@@ -22,20 +25,20 @@ module.exports = async (req, res) => {
 
                 if (rows.length > 0) {
                     await clientDB.query('ROLLBACK');
-                    const conflict = rows[0];
                     return res.status(400).json({
                         success: false,
                         conflict: {
-                            date: conflict.date,
-                            time: conflict.time,
-                            speaker: conflict.speaker,
-                            room: conflict.room,
-                            client: conflict.client
+                            date: rows[0].date,
+                            time: rows[0].time,
+                            speaker: rows[0].speaker,
+                            room: rows[0].room,
+                            client: rows[0].client
                         }
                     });
                 }
             }
 
+            // Inserir reunião
             const insertQuery = `
                 INSERT INTO meetings (date, time, duration, sector, speaker, room, client) 
                 VALUES ($1, $2, $3, $4, $5, $6, $7)
@@ -52,16 +55,17 @@ module.exports = async (req, res) => {
         } finally {
             clientDB.release();
         }
+
     } else if (req.method === 'PUT') {
-        // Lógica de reagendamento
+        // Reagendamento de reunião
         const { id, newDate } = req.body;
-        const clientDB = await db.connect();
 
         try {
             await clientDB.query('BEGIN');
 
             const [date, time] = newDate.split(' ');
 
+            // Verificar conflito de horários
             const conflictQuery = `
                 SELECT * FROM meetings 
                 WHERE date = $1 AND time = $2 AND id <> $3
@@ -73,6 +77,7 @@ module.exports = async (req, res) => {
                 return res.status(400).json({ error: 'Horário já ocupado' });
             }
 
+            // Atualizar a reunião com a nova data e hora
             const updateQuery = `
                 UPDATE meetings 
                 SET date = $1, time = $2 
@@ -82,6 +87,7 @@ module.exports = async (req, res) => {
 
             await clientDB.query('COMMIT');
             res.json({ success: true, message: 'Reunião reagendada com sucesso!' });
+
         } catch (err) {
             await clientDB.query('ROLLBACK');
             console.error('Erro ao reagendar reunião:', err);
@@ -89,6 +95,7 @@ module.exports = async (req, res) => {
         } finally {
             clientDB.release();
         }
+
     } else {
         res.status(405).json({ error: 'Método não permitido' });
     }
